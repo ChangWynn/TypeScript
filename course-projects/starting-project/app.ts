c33321eb716d540959e6c03c761f3aabd1dedfc3
ChangWynn <huynhchang.one@gmail.com>
2023-06-07 23:50:22 +0100

    function type

--- course-projects/starting-project/app.ts
+++ course-projects/starting-project/app.ts
@@ -1,5 +1,14 @@
-const add = (n1: number, n2: number, result: boolean, text: string) => {
-  const total = n1 + n2
-  if (result) { console.log(text + total) }
-  else { return total }
+const add = (n1: number, n2: number):number => {
+  return n1 + n2
 }
+
+const printResult = (num: number):void => {
+  console.log('Result: ' + num)
+}
+
+const returnResult = (num: number):undefined => {
+  console.log('Result: ' + num)
+  return;
+}
+
+printResult(add(5, 5))
