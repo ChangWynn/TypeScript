3c9bbfbebb234d06a05a913835b0034fdb39527d
ChangWynn <huynhchang.one@gmail.com>
2023-06-07 22:29:55 +0100

    added any type

--- course-projects/starting-project/app.ts
+++ course-projects/starting-project/app.ts
@@ -1,2 +1,11 @@
-
-
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: 'Chang',
+  age: 35,
+  hobbies: ['sports', 'cooking'],
+  role: [2, 'author'] // (number | string)[]
+}
