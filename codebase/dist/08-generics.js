c17dd46da00db780b6c3a3febfa65938f24a90c8
ChangWynn <huynhchang.one@gmail.com>
2023-11-13 21:48:41 +0000

    update READMEs

--- codebase/dist/08-generics.js
+++ codebase/dist/08-generics.js
@@ -4,7 +4,16 @@ const merge = (objA, objB) => {
     return Object.assign(objA, objB);
 };
 const mergedObj = merge({ name: "Max", hobbies: ["coding"] }, { age: 30, children: false });
-console.log(mergedObj.name);
-console.log(mergedObj.hobbies);
-console.log(mergedObj.age);
-console.log(mergedObj.children);
+const countAndDescribe = (element) => {
+    let description = "Got no value.";
+    if (element.length === 1) {
+        description = "Got 1 element.";
+    }
+    else if (element.length > 1) {
+        description = `Got ${element.length} elements.`;
+    }
+    return description;
+};
+console.log(countAndDescribe("Hi, There!")); // Got 10 elements
+console.log(countAndDescribe([1, 2, 3, 4, 5])); // Got 5 elements
+console.log(countAndDescribe({ name: "Max", age: 10 })); // Got no value // compilation error
--- codebase/src/08-generics.ts
+++ codebase/src/08-generics.ts
@@ -6,10 +6,29 @@ const merge = <T extends object, U extends object>(objA: T, objB: U) => {
 
 const mergedObj = merge({ name: "Max", hobbies: ["coding"] }, { age: 30, children: false });
 
-console.log(mergedObj.name);
-console.log(mergedObj.hobbies);
-console.log(mergedObj.age);
-console.log(mergedObj.children);
+// console.log(mergedObj.name);
+// console.log(mergedObj.hobbies);
+// console.log(mergedObj.age);
+// console.log(mergedObj.children);
 
 type StringArray = string[];
 type GenericStringArray = Array<string>;
+
+interface Lengthy {
+  length: number;
+}
+
+const countAndDescribe = <T extends Lengthy>(element: T) => {
+  let description = "Got no value.";
+  if (element.length === 1) {
+    description = "Got 1 element.";
+  } else if (element.length > 1) {
+    description = `Got ${element.length} elements.`;
+  }
+
+  return description;
+};
+
+console.log(countAndDescribe("Hi, There!")); // Got 10 elements
+console.log(countAndDescribe([1, 2, 3, 4, 5])); // Got 5 elements
+console.log(countAndDescribe({ name: "Max", age: 10 })); // Got no value // compilation error
